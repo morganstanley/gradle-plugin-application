db1166e2049ca91d9150b8a8a97f4c84b335c951
Denes Daniel <pantherdd@gmail.com>
2023-09-15 14:41:27 +0200

    Ensure that attribute sets are unique

---
+++ src/main/java/com/ms/gradle/application/ApplicationName.java
@@ -0,0 +1,17 @@
+package com.ms.gradle.application;
+
+import org.gradle.api.Named;
+import org.gradle.api.attributes.Attribute;
+import org.gradle.api.attributes.LibraryElements;
+import org.gradle.api.attributes.TestSuiteName;
+
+/**
+ * Attribute to define the application name. This attribute is usually found on variants that have the
+ * {@link LibraryElements} attribute valued at {@value Application#LIBRARY_ELEMENTS_APPLICATION_JAR}.
+ *
+ * @see TestSuiteName
+ */
+public interface ApplicationName extends Named {
+    Attribute<ApplicationName> APPLICATION_NAME_ATTRIBUTE =
+            Attribute.of("com.ms.gradle.application.name", ApplicationName.class);
+}
